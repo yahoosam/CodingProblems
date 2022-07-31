ad89316860388d62e689d8f30282ddebbb9742d8
yahoosam <yahoosam@gmail.com>
2022-07-30 21:56:42 -0400

    Diamond problem :pencil:

--- DiamondProblem/src/main/java/com/problems/D.java
+++ DiamondProblem/src/main/java/com/problems/D.java
@@ -1,11 +1,11 @@
 package com.problems;
 
 /*
-All the above 3 interfaces has implementation of default m1() method
+All the 3 interfaces A, B & C has implementation of default method1() method. Interface B and C extends interface A
 
 Class D implements interface B & C, now D is unable to determine which version of method1() to use.
 Whether to use Interface B's default method1() or C's default method method1()
-In this case, D class should override m1() to avoid error.
+In this case, D class should override method1() to avoid error.
 
 Through this way multiple inheritance through Interfaces are possible but not allowed with Class.
 */
